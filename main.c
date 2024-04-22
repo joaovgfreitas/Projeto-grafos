867d670994ca8e50c58691e86fbed8a890b7f7f7
Vinicius Pessoa <100362556+vspessoa0@users.noreply.github.com>
2024-04-22 14:28:13 -0300

    implementei a leitura e escrita da matriz

--- main.c
+++ main.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define MAX 100
 
 int main(int argc, char *argv[]) {
 	FILE *file;
-	file = fopen("teste.txt", "r");
+	int  i = 0;
+	char matriz[MAX][MAX], arquivo[30], frase[100];
 	
-	if(file == NULL){
-		printf("Nao foi possivel abrir arquivo.");
-		getchar();
-		exit(0);
+	printf("Digite o nome do arquivo que deseja ler ou SAIR para terminar o programa: ");
+	scanf("%s", &arquivo);
+	
+	while(strcmp(arquivo, "SAIR") != 0){
+		
+		snprintf(arquivo, sizeof(arquivo), "%s.txt",arquivo);
+		file = fopen(arquivo, "r");
+			
+		if(file == NULL){
+			printf("Nao foi possivel abrir arquivo.");
+		}
+	
+		while(fgets(frase, 100, file) != NULL){
+			strcpy(matriz[i], frase);	
+			printf("%s", matriz[i]);
+			i++;
+		}
+	
+		fclose(file);
+		printf("\n\n");
+		
+		printf("Digite o nome do arquivo que deseja ler ou SAIR para terminar o programa: ");
+		scanf("%s", &arquivo);
 	}
 	
-	char frase[100];
-	
-	while(fgets(frase, 100, file) != NULL){
-		printf("%s", frase);
-	}
-	
-	fclose(file);
-	printf("\n");
-	system("pause");
+	printf("\nSaindo...\n");
 	return 0;
 }
 
